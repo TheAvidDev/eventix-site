b1bfaf0bda228906e1ea8f82723862840b05f56d
TheAvidDev <AvidDev@protonmail.com>
2019-09-09 19:11:37 -0400

    Added pictures

--- djangostripe/settings.py
+++ djangostripe/settings.py
@@ -124,7 +124,5 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-try:
-    from local_settings import *
-except ImportError:
-    pass
+STRIPE_SECRET_KEY = '**redacted**'
+STRIPE_PUBLISHABLE_KEY = '**redacted**'
